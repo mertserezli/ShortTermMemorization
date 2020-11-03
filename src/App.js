36b52397f4f28b5b5be197fa4c6899a36c57936d
Mert Serezli <mertserezli@gmail.com>
2020-11-03 20:29:44 +0300

    export all cards button

--- src/App.js
+++ src/App.js
@@ -256,6 +256,24 @@ function CardManager() {
         await path.doc(cardId).delete();
     };
 
+    const exportToJson = (object)=>{
+        let filename = "export.json";
+        let contentType = "application/json;charset=utf-8;";
+        object = object.map(card =>{ return {front:card.front, back:card.back}});
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            var blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(object)))], { type: contentType });
+            navigator.msSaveOrOpenBlob(blob, filename);
+        } else {
+            var a = document.createElement('a');
+            a.download = filename;
+            a.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(object));
+            a.target = '_blank';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+
     return(<div>
         <h1>All Cards</h1>
         <table>
@@ -277,6 +295,9 @@ function CardManager() {
                 </tr>)}
             </tbody>
         </table>
+        <button onClick={() => exportToJson(cards)}>
+            Export
+        </button>
     </div>)
 }
 
