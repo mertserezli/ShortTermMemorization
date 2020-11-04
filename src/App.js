9188a9a833464b1fc4547bd2c159a16500e9dcc2
Mert Serezli <mertserezli@gmail.com>
2020-11-04 15:37:55 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -263,10 +263,10 @@ function CardManager() {
         let contentType = "application/json;charset=utf-8;";
         object = object.map(card =>{ return {front:card.front, back:card.back}});
         if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-            var blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(object)))], { type: contentType });
+            let blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(object)))], { type: contentType });
             navigator.msSaveOrOpenBlob(blob, filename);
         } else {
-            var a = document.createElement('a');
+            let a = document.createElement('a');
             a.download = filename;
             a.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(object));
             a.target = '_blank';
