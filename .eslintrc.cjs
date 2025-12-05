1db13e1233b7a2a48aa5a05853ab2cd23542e0a4
Mert Serezli <mertserezli@gmail.com>
2025-12-05 15:46:13 +0300

    add eslint prettier husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+  },
+};
