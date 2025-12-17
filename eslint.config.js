b8c92e2379b186cf6551471b9fae1bb137bc50e4
Mert Serezli <mertserezli@gmail.com>
2025-12-17 13:44:22 +0300

    eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,24 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    extends: [
+      js.configs.recommended,
+      pluginReact.configs.flat.recommended,
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ]);
